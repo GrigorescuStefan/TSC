2b9e05880a7eb8bb48a666450cde111d347260ac
GrigorescuStefan <stefan.grigorescu@student.unitbv.ro>
2024-03-11 09:46:47 +0200

    Rezolvat bugul cu impartirea la zero

--- lab2/lab01_testbench-interface/instr_register_test.sv
+++ lab2/lab01_testbench-interface/instr_register_test.sv
@@ -20,7 +20,12 @@ module instr_register_test
 
   timeunit 1ns/1ns;
 
+  parameter READ_NR = 20;
+  parameter WRITE_NR = 20;
+
   int seed = 555;
+  int expected_result = 0; // rezultat intern folosit la calcul
+
 
   initial begin
     $display("\n\n***********************************************************");
@@ -39,7 +44,8 @@ module instr_register_test
 
     $display("\nWriting values to register stack...");
     @(posedge clk) load_en = 1'b1;  // enable writing to register
-    repeat (3) begin
+    // repeat (3) begin - 11/03/2024 - GS
+    repeat (WRITE_NR) begin
       @(posedge clk) randomize_transaction;
       @(negedge clk) print_transaction;
     end
@@ -47,7 +53,8 @@ module instr_register_test
 
     // read back and display same three register locations
     $display("\nReading back the same register locations written...");
-    for (int i=0; i<=2; i++) begin
+    // for (int i=0; i<=2; i++) begin - 11/03/2024 - GS
+    for (int i=0; i<=READ_NR; i++) begin
       // later labs will replace this loop with iterating through a
       // scoreboard to determine which addresses were written and
       // the expected values to be read back
@@ -90,7 +97,17 @@ module instr_register_test
     $display("Read from register location %0d: ", read_pointer);
     $display("  opcode = %0d (%s)", instruction_word.opc, instruction_word.opc.name);
     $display("  operand_a = %0d",   instruction_word.op_a);
-    $display("  operand_b = %0d\n", instruction_word.op_b);
+    $display("  operand_b = %0d", instruction_word.op_b);
+    $display("  result = %0d\n", instruction_word.res);
   endfunction: print_results
+
+  function void check_results; // din instruction word luam operand A, operand B, op code si calculam iar valorile si le verificam fata de cele din DUT (case)
+    if(opcode.name == "ZERO")
+      expected_result = 0;
+    else if(opcode.name == "PASSA")
+      expected_result = operand_a;
+    else if(opcode.name == "PASSB")
+      expected_result = operand_b; 
+  endfunction check_results
 
 endmodule: instr_register_test
